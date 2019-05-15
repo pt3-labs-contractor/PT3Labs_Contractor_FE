5924271242b6621ffc69116e15bfe4ea40f29b20
echopecho <pbsmithtbn@gmail.com>
2019-05-14 22:05:44 -0500

    Render the days of the month

--- client/src/components/calendar/Calendar.js
+++ client/src/components/calendar/Calendar.js
@@ -18,11 +18,31 @@ function Calendar() {
       )
     }
     return <div>{days}</div>
+  };
+
+  function renderDaysOfMonth() {
+    const startMonth = dateFns.startOfMonth(selectedMonth);
+    const endMonth = dateFns.endOfMonth(selectedMonth);
+    const startCalendar = dateFns.startOfWeek(startMonth);
+    const endCalendar = dateFns.endOfWeek(endMonth);
+
+    let day = startCalendar;
+    let days = [];
+
+    while(day <= endCalendar) {
+      days.push(
+        <div>{dateFns.format(day, 'D')}</div>
+      )
+      day = dateFns.addDays(day, 1);
+    }
+
+    return <div>{days}</div>
   }
 
   return (
     <div>
       {renderDaysOfWeek()}
+      {renderDaysOfMonth()}
     </div>
   )
 }
