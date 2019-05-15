2cd0c0ea3753e10a2e34cd3bda2941eb2adb36cc
echopecho <pbsmithtbn@gmail.com>
2019-05-14 21:49:08 -0500

    Render the days of the week

--- client/src/components/calendar/Calendar.js
+++ client/src/components/calendar/Calendar.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import dateFns from 'date-fns';
 
 function Calendar() {
+  const [ selectedMonth, setMonth ] = useState(new Date());
+  const [ selectedDay, setDay ] = useState(new Date());
+
+  function renderDaysOfWeek() {
+    let days = [];
+
+    let start = dateFns.startOfWeek(selectedMonth);
+
+    for(let i = 0; i < 7; i++) {
+      days.push(
+        <div>
+          {dateFns.format(dateFns.addDays(start, i), 'dddd')}
+        </div>
+      )
+    }
+    return <div>{days}</div>
+  }
+
   return (
     <div>
-      
+      {renderDaysOfWeek()}
     </div>
   )
 }
