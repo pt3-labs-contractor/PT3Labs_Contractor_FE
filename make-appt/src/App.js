73c8be11ec8c1c4f933d8cc9d5aa024b8d5774eb
Bao Pham <ryanvuepham@gmail.com>
2019-05-17 22:38:23 -0500

    client can make appointment

--- make-appt/src/App.js
+++ make-appt/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import BigCalendar from 'react-big-calendar'
 
 import moment from 'moment'
+import events from './components/events'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 import axios from 'axios'
 
@@ -21,15 +22,35 @@ const localizer = BigCalendar.momentLocalizer(moment)
 // 		/>
 // 	</div>
 //)
+
+const propTypes = {}
+
 class App extends Component {
-	constructor() {
-		super()
-		//Declare state variables here
+	constructor(props) {
+		super(props)
+
+		this.state = { events }
+	}
+
+	handleSelect = ({ start, end }) => {
+		const title = window.prompt('New Event name')
+		if (title)
+			this.setState({
+				events: [
+					...this.state.events,
+					{
+						start,
+						end,
+						title
+					}
+				]
+			})
 	}
 
 	componentDidMount() {
 		//Fetch events from database here
 	}
+
 	render() {
 		//const { localizer } = this.props
 		//	const localizer = BigCalendar.momentLocalizer(moment)
@@ -39,12 +60,16 @@ class App extends Component {
 				<h1 className="App-title">Appointment Calendar</h1>
 				<div style={{ height: 950 }}>
 					<BigCalendar
+						selectable
 						localizer={localizer}
-						events={cal_events}
+						events={this.state.events}
+						//	events={cal_events}
 						step={30}
 						defaultView="week"
 						views={['month', 'week', 'day']}
 						defaultDate={new Date()}
+						onSelectEvent={event => alert(event.title)}
+						onSelectSlot={this.handleSelect}
 					/>
 				</div>
 			</div>
