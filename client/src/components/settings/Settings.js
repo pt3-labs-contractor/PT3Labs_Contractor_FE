ff8000ea7b76809a74fc7a8bc3810fce26e519cb
dlimla <danielhlim1990@gmail.com>
2019-05-22 22:29:35 -0700

    fleshed out more of the settings page with icons forms

--- client/src/components/settings/Settings.js
+++ client/src/components/settings/Settings.js
@@ -1,11 +1,37 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { IoIosTrash } from 'react-icons/io'
+import { IoMdCreate } from 'react-icons/io'
+
+
 
 function ContractorList(props) {
   return (
     <div>
-        <h2>Contractor Setting Page</h2>
+        <h2>Contractor Setting Page</h2>{"\n"}
+        <form>
+          Contractor Email<input
+            value="contractor Email"
+          />
+          Contractor Phone<input
+            value="contractor phonenumber"
+          />
+          Old Password<input/>
+          New Passowrd<input/>
+          <button>Save</button>
+        </form>
+        <form>
+          Add Service <input placeholder="Service"/>
+          <input placeholder="Price"/>
+        </form>
+        <ul>LIST OF SERVICES
+          <li>Service1<button><IoMdCreate/></button> <button><IoIosTrash/></button></li>
+          <li>Service2<button><IoMdCreate/></button> <button><IoIosTrash/></button></li>
+          <li>Service3<button><IoMdCreate/></button> <button><IoIosTrash/></button></li>
+          <li>Service4<button><IoMdCreate/></button> <button><IoIosTrash/></button></li>
+
+        </ul>
     </div>
   )
 }
