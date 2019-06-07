c37298d64b0b8c06efa1056dfdebd32adbd4834f
echopecho <pbsmithtbn@gmail.com>
2019-06-06 23:25:35 -0500

    Refactor Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import { withRouter } from 'react-router';
 import { connect } from 'react-redux';
 import './App.css';
@@ -13,9 +13,8 @@ import Login from './components/login/Login';
 import Redirect from './components/login/Redirect';
 import Register from './components/register/Register';
 import Calendar from './components/calendar/Calendar';
-// import MainNavbar from './components/navbar/MainNavbar';
-// import NavBarUser from './components/navbar/NavBarUser';
-// import NavBarContractor from './components/navbar/NavBarContractor';
+import NavBarUser from './components/navbar/NavBarUser';
+import NavBarContractor from './components/navbar/NavBarContractor';
 import Settings from './components/settings/Settings';
 import ContractorFeedback from './components/feedback/ContractorFeedback';
 import UserFeedback from './components/feedback/UserFeedback';
@@ -28,38 +27,43 @@ function App(props) {
 
   return (
     <div className="App">
-      {/* <MainNavbar /> */}
       <main className="main-content">
-        {/* <NavBarUser />
-        <NavBarContractor /> */}
-        <Switch>
-          <Route exact path="/" component={Homepage} />
-          <Route path="/users" component={Users} />
-          <Route exact path="/contractors" component={ContractorList} />
-          <Route path="/contractors/:id" component={Contractor} />
-          <Route path="/login" component={Login} />
-          <Route path="/redirect" component={Redirect} />
-          <Route exact path="/register" component={Register} />
-          <Route
-            path="/register/oauth"
-            render={props => <Register {...props} oauth />}
-          />
-          <Route
-            path="/calendar"
-            render={props => <Calendar contractor={{}} />}
-          />
-          <Route path="/settings" component={Settings} />
-          <Route path="/contractorFeedback" component={ContractorFeedback} />
-          <Route path="/userFeedback" component={UserFeedback} />
-        </Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route
+          path="/app"
+          component={props.user.contractorId ? NavBarContractor : NavBarUser}
+        />
+        <Route path="/users" component={Users} />
+        <Route exact path="/app/contractors" component={ContractorList} />
+        <Route path="/app/contractors/:id" component={Contractor} />
+        <Route path="/login" component={Login} />
+        <Route path="/redirect" component={Redirect} />
+        <Route exact path="/register" component={Register} />
+        <Route
+          path="/register/oauth"
+          render={props => <Register {...props} oauth />}
+        />
+        <Route
+          path="/calendar"
+          render={props => <Calendar contractor={{}} />}
+        />
+        <Route path="/settings" component={Settings} />
+        <Route path="/contractorFeedback" component={ContractorFeedback} />
+        <Route path="/userFeedback" component={UserFeedback} />
       </main>
     </div>
   );
 }
 
+const mapStateToProps = state => {
+  return {
+    user: state.user,
+  };
+};
+
 export default withRouter(
   connect(
-    null,
+    mapStateToProps,
     { fetchAccts }
   )(App)
 );
