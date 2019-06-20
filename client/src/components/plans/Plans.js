ada56bc11679105543765c3a0f242e02be0535e1
ltann <leeroyleeroy18@gmail.com>
2019-06-20 00:00:18 -0700

    styled contractor feedback page and revamped pricing page

--- client/src/components/plans/Plans.js
+++ client/src/components/plans/Plans.js
@@ -10,12 +10,19 @@ export default function Plans() {
       <TopNavbar />
       <NavBarContractor />
       <div className="main-body">
-        <h1>Plans Page</h1>
+        <div className="main-header">
+          <h2 className="main-header-title">Pricing</h2>
+          <p>
+            Please choose a plan that best fits your need. Here at Digital
+            Calendar we are always looking to provide the best value to our
+            customers.
+          </p>
+        </div>
         <div className="wrapper">
           <div className="single-price">
             <h2>Basic</h2>
             <div className="price">
-              <h2>Free</h2>
+              <h3>Free</h3>
             </div>
             <div className="deals">
               <h4>Up to 5 hrs</h4>
@@ -24,13 +31,13 @@ export default function Plans() {
               <h4>Up to 5 hrs</h4>
               <h4>Up to 5 hrs</h4>
             </div>
-            <Link>Select</Link>
+            <Link>Free</Link>
           </div>
 
           <div className="single-price">
             <h2>Premium</h2>
             <div className="price">
-              <h2>$19.99</h2>
+              <h3>$19.99</h3>
             </div>
             <div className="deals">
               <h4>Unlimted Everything!</h4>
