4d4a70c037d72312e71020107dffe632f10c6bc1
Daniel Lim <danheelim@gmail.com>
2019-07-11 20:13:54 -0700

    finishes the css of user setting page

--- client/src/components/settings/UserSettings.js
+++ client/src/components/settings/UserSettings.js
@@ -31,43 +31,48 @@ function UserSetting(props) {
       <NavBarUser />
       <div className="main-container">
         <div className="settings-container">
-          <h2>{props.User.username} Setting Page</h2>
+          <h2 className="title">{props.User.username} Settings</h2>
           {'\n'}
           <form onSubmit={handleUpdate}>
+              <div className="email-div">
+                Email
+                <input
+                  className="email-input"
+                  value={email}
+                  type="text"
+                  name="userEmail"
+                  onChange={e => setEmail(e.target.value)}
+                  />
+              </div>
 
-              Email
-              <input
-                className="email-input"
-                value={email}
-                type="text"
-                name="userEmail"
-                onChange={e => setEmail(e.target.value)}
-                />
-   
-
-
-            Username
-            <input
-              value={username}
-              type="text"
-              name="userUsername"
-              onChange={e => setUsername(e.target.value)}
-              />
+              <div className="username-div">
+                Username
+                <input
+                  value={username}
+                  type="text"
+                  name="userUsername"
+                  onChange={e => setUsername(e.target.value)}
+                  />
+                </div>
             {/* Old Password
             <input />
 
             New Passowrd
             <input /> */}
-            Phone Number
-            <input
-              value={phoneNumber}
-              type="text"
-              name="userphoneNumber"
-              onChange={e => setPhoneNumber(e.target.value)}
-            />
+            <div className="phonenumber-div">
+                Phone Number
+                <input
+                  value={phoneNumber}
+                  type="text"
+                  name="userphoneNumber"
+                  onChange={e => setPhoneNumber(e.target.value)}
+                  />
+            </div>
+
             <div class="box">
               <button  className="btn btn-three">Save</button>
             </div>
+
           </form>
 
           {/* <form>
