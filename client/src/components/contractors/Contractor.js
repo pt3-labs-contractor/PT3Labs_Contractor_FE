7b55eb72cb6b49b347183dea504f02739d0f19fd
Daniel Lim <danheelim@gmail.com>
2019-07-25 18:52:17 -0700

    some changes for contractor.js

--- client/src/components/contractors/Contractor.js
+++ client/src/components/contractors/Contractor.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 
 import Calendar from '../calendar/Calendar';
