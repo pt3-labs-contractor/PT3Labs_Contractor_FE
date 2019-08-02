103d716635a4900a866826d6d9fa00974b395de8
Phillip Smith <41526919+echopecho@users.noreply.github.com>
2019-08-02 02:31:22 -0500

    Update Register.js

--- client/src/components/register/Register.js
+++ client/src/components/register/Register.js
@@ -60,6 +60,7 @@ function Register(props) {
           )
           .then(res => {
             userUpdate.contractorId = res.data.id;
+            console.log(userUpdate);
             axios.put(
               'https://fierce-plains-47590.herokuapp.com/api/users',
               userUpdate,
